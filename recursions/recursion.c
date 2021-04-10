b441eb0dee4144903f205d7ffc7f935b2ddf6446
commandbear <yuripedrorj@hotmail.com>
2021-04-10 17:56:25 -0300

    Inserção do fatorial iterativo

--- recursions/recursion.c
+++ recursions/recursion.c
@@ -14,7 +14,12 @@ double fat_ternary (double n){
 }
 
 double fat_itaractive (double n){
-
+    double res = 1;
+    int counter = n;
+    for(counter = n; counter > 1; counter--){
+        res *= counter;
+    }
+    return res;
 }
 
 double fib_rec(double n){
@@ -25,15 +30,31 @@ double fib_rec(double n){
     }
 }
 
+double fib_iteractive (double n){
+    if (n<=2){
+        return 1;
+    }
+    
+    double penult = 1, last = 1, current = 2, counter, aux; //explicação current: 1h:05:32m - Aula 5
+    
+    for(counter = 3; counter < n; counter++){
+        aux = last;
+        current += last;    
+        last += penult;
+        penult = aux;
+    }
+    return current;
+}
+
 int main(){
 
     double value;
-    //printf("Insert the value to calc the fatorial:");
     printf("Insert the value to calc the fibonacci:");
     scanf("%lf", &value);
 
-    //printf("Fatorial with Recursion without Ternaty: %lf\n", fat_recursive(value));
-    //printf("Fatorial with Recursion withou Ternaty:");
-    printf("Fibonacci Recursive: %lf\n", fib_rec(value));
+    printf("Recursive Factorial without Ternary: %.0lf\n", fat_recursive(value));
+    printf("Recursive Factorial with Ternary: %.0lf\n", fat_ternary(value));
+    printf("Fibonacci Recursive: %.0lf\n", fib_rec(value));
+    printf("Fibonacci Iteractive: %.0lf\n", fib_iteractive(value));
     return 0;
 }
