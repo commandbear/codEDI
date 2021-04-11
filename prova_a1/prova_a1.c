6a05898a0fe058367e5f204ec4d253dba1fffb43
commandbear <yuripedrorj@hotmail.com>
2021-04-11 09:16:24 -0300

    limpeza de comentários inúteis

--- prova_a1/prova_a1.c
+++ prova_a1/prova_a1.c
@@ -26,74 +26,24 @@ void reports(Student *s){
     int i;
     int highest_grade = 0, lowest_grade = 0;
 
+    
     for(i = 0; i < TOTSTUDENTS; i++){
         printf("==========================\n");
-        printf("%d Student ID: %ld\n", s[i].code);
-        printf("%d Name: %s\n", s[i].name);
+        printf("\nCURRENTY: %d\n", i);
+        printf("Student ID: %ld\n", s[i].code);
+        printf("Name: %s\n", s[i].name);
         printf("Grade 1: %.1f\n", s[i].grade1);
         printf("Grade 2: %.1f\n", s[i].grade2);
         printf("Grade 3: %.1f\n", s[i].grade3);
-/*
-        if(s[i].grade1 > s[i].grade2 > s[i].grade3 || s[i].grade1 > s[i].grade3 > s[i].grade2){
-            highest_grade = i;
-        } */
-    }
 
-    //captando o valor do indice    
-    for(i = 1; i < TOTSTUDENTS; i++){
-        /*
-        if(s[i].grade1 > s[i].grade2 && s[i].grade2 > s[i].grade3){
-            highest_grade = s[i].grade1; //valor mais alto é atribuido à variável highest_grade (nota mais alta)
-            lowest_grade = s[i].grade3;
-        } else if(s[i].grade2 > s[i].grade1 && s[i].grade1 > s[i].grade3  ){
-            highest_grade = s[i].grade2;
-            lowest_grade = s[i].grade3;
-        } else if(s[i].grade3 > s[i].grade2 && s[i].grade2 > s[i].grade1){
-            highest_grade = s[i].grade3;
-            lowest_grade = s[i].grade1;
-        } else if(s[i].grade1 > s[i].grade3 && s[i].grade3 > s[i].grade2){
-            highest_grade = s[i].grade1;
-            lowest_grade = s[i].grade2;
-        } else if(s[i].grade2 > s[i].grade3 && s[i].grade3 > s[i].grade1){
-            highest_grade = s[i].grade2;
-            lowest_grade = s[i].grade1;
-        } else if(s[i].grade3 > s[i].grade1 && s[i].grade1 > s[i].grade2){
-            highest_grade = s[i].grade3;
-            lowest_grade = s[i].grade2;
-        }*/
-        if(s[i].grade1 > s[i].grade2 && s[i].grade1 > s[i].grade3){
+        if(s[i].grade1 > s[highest_grade].grade2 && s[i].grade1 > s[highest_grade].grade3){
             highest_grade = i;
-            printf("CURRENTY: %d", i);
+            printf("\nCURRENTY i: %d\n CURRENTY HIGHEST GRADE: %d\n", i, highest_grade);
         } 
     }
-    //captando o índice
-    /*
-    for(i = 0; i < TOTSTUDENTS; i++){
-        if(s[i].grade1 > s[i].grade2 && s[i].grade2 > s[i].grade3){
-            highest_grade = i; 
-            lowest_grade = i;
-        } else if(s[i].grade2 > s[i].grade1 && s[i].grade1 > s[i].grade3){
-            highest_grade = i;
-            lowest_grade = i;
-        } else if(s[i].grade3 > s[i].grade2 && s[i].grade2 > s[i].grade1){
-            highest_grade = i;
-            lowest_grade = i;
-        } else if(s[i].grade1 > s[i].grade3 && s[i].grade3 > s[i].grade2){
-            highest_grade = i;
-            lowest_grade = i;
-        } else if(s[i].grade2 > s[i].grade3 && s[i].grade3 > s[i].grade1){
-            highest_grade = i;
-            lowest_grade = i;
-        } else if(s[i].grade3 > s[i].grade1 && s[i].grade1 > s[i].grade2){
-            highest_grade = i;
-            lowest_grade = i;
-        }
-    }*/
-    //printf("Without INDEX: The highest grade is %.1f\n", highest_grade);
-    //printf("Without INDEX: The lowest grade is %.1f\n", lowest_grade);
+   
     printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
     printf("The highest first grade is %.1f\n", s[highest_grade].grade1);
-    //printf("With INDEX: The lowest grade is %.1f\n", s[i].grade1);
 }
 
 int main(){
