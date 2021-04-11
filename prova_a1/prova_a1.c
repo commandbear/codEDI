24d31aa50fd828b33fc7764cfb171b87d54818cf
commandbear <yuripedrorj@hotmail.com>
2021-04-11 00:46:37 -0300

    captação da maior e menor nota #semteste

--- prova_a1/prova_a1.c
+++ prova_a1/prova_a1.c
@@ -23,13 +23,38 @@ void populateStudents (Student *s){
 }
 
 void reports(Student *s){
-    for(int i = 0; i < TOTSTUDENTS; i++){
+    int i;
+    float highest_grade = 0, lowest_grade = 0;
+
+    for(i = 0; i < TOTSTUDENTS; i++){
         printf("==========================\n");
         printf("%d Student ID: %ld\n", i+1, s[i].code);
         printf("%d Name: %s\n", i+1, s[i].name);
         printf("Grade 1: %.1f\n", s[i].grade1);
         printf("Grade 2: %.1f\n", s[i].grade2);
         printf("Grade 3: %.1f\n", s[i].grade3);
+    }
+
+    for(i = 0; i < TOTSTUDENTS; i++){
+        if(s[i].grade1 > s[i].grade2 && s[i].grade2 > s[i].grade3){
+            highest_grade = s[i].grade1; //valor mais alto é atribuido à variável highest_grade (nota mais alta)
+            lowest_grade = s[i].grade3
+        } else if(s[i].grade2 > s[i].grade1 && s[i].grade1 > [i].grade3  ){
+            highest_grade = s[i].grade2;
+            lowest_grade = s[i].grade3;
+        } else if(s[i].grade3 > s[i].grade2 && s[i].grade2 > s[i].grade1){
+            highest_grade = s[i].grade3;
+            lowest_grade = s[i].grade1;
+        } else if(s[i].grade1 > s[i].grade3 && s[i].grade3 > s[i].grade2){
+            highest_grade = s[i].grade1;
+            lowest_grade = s[i].grade2;
+        } else if(s[i].grade2 > s[i].grade3 && s[i].grade3 > s[i].grade1){
+            highest_grade = s[i].grade2;
+            lowest_grade = s[i].grade1;
+        } else if(s[i].grade3 > s[i].grade1 && s[i].grade1 > s[i].grade2){
+            highest_grade = s[i].grade3;
+            lowest_grade = s[i].grade2;
+        }
     }
 }
 
@@ -38,6 +63,6 @@ int main(){
 
     populateStudents(students); //passa os parâmetros do vetor para a função
     reports(students);
-    
+
     return 0;
 }
