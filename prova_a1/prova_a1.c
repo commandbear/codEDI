22e94f0ef4c0234eda4e7c72d1c17f600633dfe9
commandbear <yuripedrorj@hotmail.com>
2021-04-18 16:24:56 -0300

    alteração na função multi da questão 5

--- prova_a1/prova_a1.c
+++ prova_a1/prova_a1.c
@@ -150,9 +150,9 @@ long multi(int a, int b){
     if(a == 0 || b == 0){
         return 0;
     } else if (b>0){
-        return a + multi(a, b-1);
+        return multi(a, b-1) + a;
     } else {
-        return -a + multi(-a, b+1);
+        return multi(a, b+1) - a;
     }
 }
 
