6ee4c7732e78dd375b5f52c684a5e3c92cedd820
commandbear <yuripedrorj@hotmail.com>
2021-06-05 15:17:38 -0300

    Inserido a função de limpeza de memória

--- queues/queues.h
+++ queues/queues.h
@@ -47,4 +47,44 @@ void insert(Tqueue *q, int element){
     if(!q->start){
         q->start = node;
     }
+
+    //remover um elemento na fila
+    int remove_node(Tqueue *q){
+        if(empty (q)){
+            exit(1);
+        }
+
+        //ponteiro q->start aponta para o primeiro elemento da fila
+        int value = q->start->info;
+
+        //criar um novo ponteiro nó que aponta para o primeiro elemento da fila, que será removido
+        Tnode *p = q->start;
+
+        //andar com o ponteiro de inicio da fila para o próximo elemento
+        q->start = q->start->next;
+
+        //apontar o ponteiro de fim da fila para NULL, caso o elemento a ser removido seja o último restante
+        if(!q->start){
+            q->end = NULL;
+        }
+
+        //liberar a memória do elemento removido da fila
+        free(p);
+
+        return value;
+    }
+
+    //limpar uma fila e liberar a sua memória e dos seus elementos
+    void clear(Tqueue *q){
+        Tnode *p = q->start, *aux;
+        //enquanto existirem elementos na fila
+        while(p){
+            aux = p;        //utilizar o auxiliar para não perder o apontamento
+            p = aux->next;  //caminhar com o ponteiro p
+            free(aux);      //liberar a memória e elemento em questão da fila
+        }
+        free(q); //liberar a memória da fila
+    }
+
+     
 }
---
+++ queues/queuesContinue.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+#include "queues.h"
+
+void menu () {
+  //int option, elem, removed;
+  int option, elem;
+  Tqueue *q = NULL;
+
+  do {
+    printf ("******* MENU DE GERENCIAMENTO DE FILAS *******\n");
+    printf ("1 - Criar uma fila vazia\n");
+    printf ("2 - Inserir um elemento na fila\n");
+    printf ("3 - Remover um elemento da fila\n");
+    printf ("4 - Verificar se a fila está vazia\n");
+    // printf ("5 - Limpar e remover a fila\n");
+    printf ("5 - Imprimir os elementos da fila\n");
+    printf ("6 - Sair\n");
+    printf ("Escolha uma opção: ");
+    scanf ("%d", &option);
+
+    switch (option) {
+      case 1:
+        if (!q) {
+          q = create();
+          printf ("Fila criada com sucesso!\n\n");
+        }
+        else {
+          printf ("Uma fila já foi criada.\n\n");
+        }
+        system("PAUSE");
+        system ("cls");
+        break;
+      case 2:
+        if (!q) {
+          printf ("A fila não existe. Crie uma fila antes de inserir os elementos\n\n");
+        }
+        else {
+          printf ("Digite um valor inteiro para o elemento a ser inserido na fila: ");
+          scanf ("%d", &elem);
+          insert (q, elem);
+          printf ("Elemento com valor %d inserido na fila com sucesso.\n\n", elem);
+        }
+        system("PAUSE");
+        system ("cls");
+        break;
+      case 3:
+        if (!q) {
+          printf ("Nao é possível remover elementos de uma fila que não existe.\n\n");
+        }
+        else if (empty(q)) {
+          printf ("Nao é possível remover elementos de uma fila vazia.\n\n");
+        }
+        else {
+          printf ("O elemento %d foi removido da fila.\n\n", remove_node(q));
+        }
+        system("PAUSE");
+        system ("cls");
+        break;
+      case 4:
+        if (empty(q)) {
+          printf ("\nA fila está vazia\n\n");
+        }
+        else {
+          printf ("\nA fila não está vazia\n\n");
+        }
+        system("PAUSE");
+        system ("cls");
+        break;
+      case 5:
+        if (!q) {
+          printf ("Nao é possível imprimir os elementos de uma fila que não existe.\n\n");
+        }
+        else if (empty (q)) {
+          printf ("Fila vazia.\n\n");
+        }
+        else {
+          printf ("Imprimindo os elementos da fila.\n");
+          print_queue (q);
+        }
+
+        system("PAUSE");
+        system ("cls");
+        break;
+      // case 5:
+      //   if (!q) {
+      //     printf ("Nao é possível liberar a memória de uma fila que não existe.\n\n");
+      //   }
+      //   else {
+      //     clear (q);
+      //     printf ("A memória da fila foi liberada com sucesso.\n\n");
+      //   }
+      //   sleep (SLEEP_TIME);
+      //   system ("clear");
+      //   break;
+      case 6:
+        // if (q->start && q->end) {
+        //   clear (q);
+        // }
+        clear (q);
+        printf ("Saindo do programa...\n\n");
+        system("PAUSE");
+        system ("cls");
+        break;
+      default:
+        printf ("Opção inválida!\n\n");
+        system("PAUSE");
+        system ("cls");
+        break;
+    }
+  } while (option != 6);
+}
+
+int main () {
+  menu();
+  return 0;
+}
