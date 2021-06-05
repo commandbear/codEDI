c0d90f00362cdbc1a92ef57bbe16de67d5ae7b3f
commandbear <yuripedrorj@hotmail.com>
2021-06-05 10:21:31 -0300

    Retorna se a fila está vazia ou não

--- queues/queues.c
+++ queues/queues.c
@@ -2,6 +2,15 @@
 #include "queues.h"
 
 int main(){
-    
+    Tqueue *q;
+    q = create();
+
+    if(empty(q)){
+        printf("Fila vazia >>>>> %d\n", empty(q));
+    }
+    else{
+        printf("Fila não vazia >>>>>> %d\n", empty(q));
+    }
+
     return 0;
 }
--- queues/queues.h
+++ queues/queues.h
@@ -6,7 +6,7 @@
 //Definição de um nó (elemento) da fila
 typedef struct node{
     int info;
-    struct node *prox;    
+    struct node *next;    
 } Tnode; //T de "tipo"
 
 //Definição da estrutura da fila
@@ -22,4 +22,26 @@ Tqueue *create(){
     //q = malloc (100 * sizeof(Tqueue));
 
     return q;
+}
+
+//Verificar se a fila está vazia
+int empty(Tqueue *q){
+    return !q->end;
+}
+
+//Inserir um elemento na fila
+void insert(Tqueue *q, int element){
+    Tnode *node = (Tnode *) malloc (sizeof(Tnode));
+    node->info = element;
+    node->next = NULL;
+
+    //Apontar o ponteiro do antigo último elemento para o novo último elemento
+    if(q->end){
+        q->end = node;
+    }
+
+
+    if(!q->start){
+        q->start = node;
+    }
 }
