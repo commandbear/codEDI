2edb786e84282f1cc64b8396e3a827ce4d5cd073
commandbear <yuripedrorj@hotmail.com>
2021-06-06 19:59:05 -0300

    Primeira implementação das funções de pilha

--- stack/stack.c
+++ stack/stack.c
@@ -3,6 +3,33 @@
 #include "stack.h"
 
 int main(){
+    Tstack *stack = create();
+    int elem;
+    
+    printf("Insira o valor de info para o elemento da pilha: ");
+    scanf("%d", &elem);
+
+    push(stack, elem);
+
+    empty(stack) ? printf("Pilha Vazia\n\n") : printf("Pilha Nao Vazia\n\n"); //ternário
+
+    /*if(empty(stack)){
+        printf("Pilha Vazia\n\n");
+    } else {
+        printf("Pilha Nao Vazia\n\n");
+    }*/
+    
+    printf("Elemento %d removido da pilha\n\n", pop(stack));
+
+    empty(stack) ? printf("Pilha Vazia\n\n") : printf("Pilha Nao Vazia\n\n"); //ternário
+
+    /*if(empty(stack)){
+        printf("Pilha Vazia\n\n");
+    } else {
+        printf("Pilha Nao Vazia\n\n");
+    }*/
+
+    clear(stack);
 
     return 0;
 }
