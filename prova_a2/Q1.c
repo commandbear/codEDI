71f78393840edcadf3b1968a853e2a8fe3e0de35
commandbear <yuripedrorj@hotmail.com>
2021-06-23 19:45:24 -0300

    Q1 concluída

--- prova_a2/Q1.c
+++ prova_a2/Q1.c
@@ -1,7 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include"stack.h"
+
+int soma_par(Tstack *p){
+    Tstack *aux = stackCreator();
+    int elem, sum = 0;
+
+    while(!emptyStack(p)){
+        push(aux, pop(p));
+    }
+
+    while (!emptyStack(aux)){
+        elem = pop(aux);
+        if(elem % 2 == 0){
+            sum += elem;
+        }
+        push(p, elem);
+    }
+    stackPrinter(p);
+
+    return sum;
+}
 
 int main(){
+    Tstack *pilha = stackCreator();
+    int elem;
+    char choose;
 
+    do{
+        printf("Digite um elemento para inserir na PILHA: ");
+        scanf("%d", &elem);
+        push(pilha, elem);
+        setbuf(stdin, NULL);
+        printf("Deseja continuar? [s/n]: ");
+        scanf("%c", &choose);
+    }while(choose != 'n');
+
+    printf("A soma dos pares da pilha eh: %d\n", soma_par(pilha));
+
+    stackCleaner(pilha);
     return 0;
 }
