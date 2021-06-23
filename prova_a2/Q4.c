3ceb3b71aa251fc16b3869e3f859ab7aa57962c2
commandbear <yuripedrorj@hotmail.com>
2021-06-22 22:30:20 -0300

    Atualização da Q4 para execução de vários sorteios

--- prova_a2/Q4.c
+++ prova_a2/Q4.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"lists.h"
+#include<windows.h>
 
 int main(){
     Tlist *sorteio;
     
     sorteio = mega_sena();
 
-    printf("\n******MEGA-SENA SORTEADA******\n");
-    print_list(sorteio);
-    printf("*****************************\n");
+    //for(int i = 1; i <= 30; i++){
+        //sorteio = mega_sena();
+        printf("\n******MEGA-SENA SORTEADA******\n");
+        print_list(sorteio);
+        printf("*****************************\n");
+        //Sleep(2000);
+    //}    
 
     clear(sorteio);
     return 0;
